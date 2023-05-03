c24baebe3b0244aa668d1e4d438e29b86d1724b2
WeslleyFelixvk <WeslleyFelixvk>
2023-05-03 00:37:20 +0000

    projetinho hoje

--- environment/mount/mountpoint.py
+++ environment/mount/mountpoint.py
@@ -18,10 +18,14 @@
 
 # COMMAND ----------
 
+scopo-kv-bootcamp-prd-wsy
+
+# COMMAND ----------
+
 dbutils.fs.mount(
-    source = "wasbs://raw@dlsbootcamptestewsy.blob.core.windows.net"
-    ,mount_point = "/mnt/raw/"
-    ,extra_configs = {"fs.azure.account.key.dlsbootcamptestewsy.blob.core.windows.net" :dbutils.secrets.get(scope = "scopo-kv-bootcamp-teste-wsy", key = "secret-dlsbootcamptestewsy")}
+    source = "wasbs://sandbox@dlsbootcampprdwsy.blob.core.windows.net"
+    ,mount_point = "/mnt/sandbox/"
+    ,extra_configs = {"fs.azure.account.key.dlsbootcampprdwsy.blob.core.windows.net" :dbutils.secrets.get(scope = "scopo-kv-bootcamp-prd-wsy", key = "secret-dlsbootcampprdwsy")}
 )
 
 # COMMAND ----------
@@ -42,7 +46,7 @@ dbutils.fs.unmount( f"/mnt/raw/")
 
 # COMMAND ----------
 
-config =  {"fs.azure.account.key.dlsbootcamptestewsy.blob.core.windows.net" :dbutils.secrets.get(scope = "scopo-kv-bootcamp-teste-wsy", key = "secret-dlsbootcamptestewsy")}
+config =  {"fs.azure.account.key.dlsbootcampprdwsy.blob.core.windows.net" :dbutils.secrets.get(scope = "scopo-kv-bootcamp-prd-wsy", key = "secret-dlsbootcampprdwsy")}
 
 # COMMAND ----------
 
@@ -57,7 +61,7 @@ def mount_diretorio_lake(lst_diretorios):
     try:        
         for diretorio in lst_diretorios:
             dbutils.fs.mount(
-                source = f"wasbs://{diretorio}@dlsbootcamptestewsy.blob.core.windows.net"
+                source = f"wasbs://{diretorio}@dlsbootcampprdwsy.blob.core.windows.net"
                 ,mount_point = f"/mnt/{diretorio}/"
                 ,extra_configs = config
             )
@@ -86,3 +90,7 @@ unmount_diretorio_lake(diretorios)
 
 # DBTITLE 1,listando diretórios no DBFS com o dbutils
 dbutils.fs.ls("/mnt/raw/apis/instrutores/")
+
+# COMMAND ----------
+
+# MAGIC %fs ls "/mnt/raw/apis/instrutores/"
